157374684828278174b87f59600ec690c7833c18
Ivan <akbars06_2010@mail>
2021-01-15 15:39:24 +0300

    finall

--- src/js/modules/modalOverlay.js
+++ src/js/modules/modalOverlay.js
@@ -4,14 +4,14 @@
  const showModal = (overlay, modal) => {
         overlay.style.display = 'block';
         modal.style.display = 'block';
-        document.body.classList.add('no-scroll');
+        document.body.style.overflow = 'hidden';
         document.body.style.marginRight = `${calcScroll()}px`
     };
 
     const hiddenModal = (overlay, modal) => {
         overlay.style.display = 'none';
         modal.style.display='none';
-        document.body.classList.remove('no-scroll');
+        document.body.style.overflow = '';
         document.body.style.marginRight = `0px`;
     };
 
